219be1a9524c05ae90ee574ad023ecb907aded64
sushilsangle <sushilsangle.ss@gmail.com>
2024-08-22 11:13:11 +0530

    added addition logic

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,10 @@
 public class HelloWorld {
     public static void main(String[] args) {
+        int a,b,c;
+        a=10;
+        b=20;
+        c=a+b;
         System.out.println("Hello, World!");
-          System.out.println("This is updated code");
+          System.out.println("The addition of two numbers is: "+c);
     }
 }
