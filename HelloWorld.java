332f4a75028701ff88392b4ebe6f8a59b8e4616f
sushilsangle <sushilsangle.ss@gmail.com>
2024-08-22 11:26:47 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,10 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        int a,b,c;
-        a=10;
-        b=20;
-        c=a+b;
+        
         System.out.println("Hello, World!");
-          System.out.println("The addition of two numbers is: "+c);
+         
     }
 }
