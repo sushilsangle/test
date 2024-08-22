748532f190e162c9c57ded0eb30b9a6e6943466c
sushilsangle <sushilsangle.ss@gmail.com>
2024-08-22 16:19:14 +0530

    addition number

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,10 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        
+        int a,b,c;
+        a=10;
+        b=20;
+        c=a+b;
         System.out.println("Hello, World!");
-         
+        System.out.println("Addityion of two numbers: "+c);
     }
 }
